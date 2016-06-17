fec37026f7a1e001e7af50c0c731be8344e0ff2b
Ivan Hayes <git@ivanhayes.com>
2016-06-17 12:22:01 +0100

    Update example app code style

--- js/app/app.js
+++ js/app/app.js
@@ -1,31 +1,28 @@
-(function ()
-{
-    "use strict";
+(function () {
+    'use strict';
 
     var swiftclick = window.SwiftClick.attach(document.body);
     swiftclick.useCssParser(true);
 
     // add regular click listeners to all elements with a class of 'test-element'.
-    var testElements = document.getElementsByClassName("test-element"),
+    var testElements = document.getElementsByClassName('test-element'),
         i = 0,
         length = testElements.length;
     
-    for (i; i < length; i++)
-    {
-        testElements[i].addEventListener ("click", elementClicked, false);
+    for (i; i < length; i++) {
+        testElements[i].addEventListener ('click', elementClicked, false);
     }
 
 
     // regular click handler which simply toggles a CSS class on clicked elements.
-    function elementClicked (event)
-    {
+    function elementClicked (event) {
         event.preventDefault();
 
-        console.log("App:: [elementClicked]");
+        console.log('App:: [elementClicked]');
 
         var currentElement = event.target;
 
-        currentElement.classList.toggle("bg-colour-change");
+        currentElement.classList.toggle('bg-colour-change');
     }
 
 })();
